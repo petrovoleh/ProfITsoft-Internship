4f36a24b7f5a7e5060d086b3c26ee0a36c0dd88f
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2024-04-06 15:08:33 +0300

    Додано коментарі в модель

--- block_1_task/src/main/java/org/example/model/Client.java
+++ block_1_task/src/main/java/org/example/model/Client.java
@@ -1,19 +1,28 @@
 package org.example.model;
 
-
+/**
+ * Цей клас представляє модель клієнта з іменем та електронною поштою.
+ */
 public class Client {
-    private String name;
-    private String email;
+    private String name; // Ім'я клієнта
+    private String email; // Електронна пошта клієнта
 
-    public Client(){}
+    /**
+     * Створює порожній об'єкт клієнта.
+     */
+    public Client() {}
 
+    /**
+     * Створює об'єкт клієнта з вказаним ім'ям та електронною поштою.
+     * @param name ім'я клієнта
+     * @param email електронна пошта клієнта
+     */
     public Client(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
-    // Getters and setters
-
+    /* Гетери і сетери */
     public String getName() {
         return name;
     }
--- block_1_task/src/main/java/org/example/model/Order.java
+++ block_1_task/src/main/java/org/example/model/Order.java
@@ -4,15 +4,28 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Цей клас представляє модель замовлення з ідентифікатором, датою замовлення, клієнтом, сумою та списком товарів.
+ */
 public class Order {
-    private int orderId;
-    private Date orderDate;
-    private String client;
-    private int amount;
-    private List<String> items;
+    private int orderId; // Ідентифікатор замовлення
+    private Date orderDate; // Дата замовлення
+    private String client; // Ім'я клієнта
+    private int amount; // Сума замовлення
+    private List<String> items; // Список товарів
 
+    /**
+     * Створює порожній об'єкт замовлення.
+     */
     public Order() {}
 
+    /**
+     * Створює об'єкт замовлення з вказаними параметрами.
+     * @param orderId ідентифікатор замовлення
+     * @param orderDate дата замовлення
+     * @param amount сума замовлення
+     * @param client ім'я клієнта
+     */
     public Order(int orderId, Date orderDate, int amount, String client) {
         this.orderId = orderId;
         this.orderDate = orderDate;
@@ -21,8 +34,7 @@ public class Order {
         this.client = client;
     }
 
-    // Getters and setters
-
+    /* Гетери і сетери */
     public int getOrderId() {
         return orderId;
     }
